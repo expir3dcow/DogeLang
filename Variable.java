3c5b696fbb6ba89a075bf8d6ccbc09a4e9fac8b3
expir3dcow <expir3dcow@gmail.com>
2015-04-16 19:32:17 +0100

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+	String name;
+	String value;
+	
+	public Variable(String name, String value){
+		this.name = name;
+		this.value = value;
+	}
+	
+	
+	public String getName(){return name;}
+	
+	public String getValue(){return value;}
+	
+}
